c4ea19b6d5498a34c89b42efb3130696940a9cd5
Mandy Petrakis <mandypetrakis@gmail.com>
2022-11-28 13:27:26 -0800

    commit tree 24c36f26d486cea928845db91178db51b03c0772

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(array, string) {
+    let match = array.filter((driver) => driver.toUpperCase() === string.toUpperCase())
+    return match
+}
+
+function fuzzyMatch(array, letter) {
+    let startsWith = array.filter((driver) => driver.startsWith(letter))
+    return startsWith
+}
+
+function matchName(array, name) {
+    let match = array.filter((driver) => driver.name === name)
+    return match
+}
